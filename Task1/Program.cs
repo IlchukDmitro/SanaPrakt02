89f4229c89fb6877d9f2101545e2418cd4307641
Ilchuk Dmitro <ipz224_ida@student.ztu.edu.ua>
2022-11-10 23:19:44 +0200

    created new branch second

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.ReadLine();
