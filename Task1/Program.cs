0aa5540744f64b80c4601be8f96901fe72687638
Ilchuk Dmitro <ipz224_ida@student.ztu.edu.ua>
2022-11-10 17:29:37 +0200

    changed the message in the last 2 lines

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,5 +1,5 @@
-﻿Console.WriteLine("Андрій Морозов Топ :)");
-Console.WriteLine("Андрій Морозов Топ :)");
-Console.WriteLine("Андрій Морозов Топ :)");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+﻿Console.WriteLine("Андрiй Морозов Топ :)");
+Console.WriteLine("Андрiй Морозов Топ :)");
+Console.WriteLine("Андрiй Морозов Топ :)");
+Console.WriteLine("ZTU The Best!");
+Console.WriteLine("ZTU One Love <3");
